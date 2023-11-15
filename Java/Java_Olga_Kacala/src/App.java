75fc1eecc83ae856bb1ebd1fa285309749b0e490
olga-kacala <olga.kacala@gmail.com>
2023-11-15 17:58:25 +0100

    Java frame

--- Java/Java_Olga_Kacala/src/App.java
+++ Java/Java_Olga_Kacala/src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        String name = "Piotr";
+        String surname = "Budynek";
+        long tel = 123456789;
+        String city = "Krakow";
+        String frame = "";
+        String sentence = "* tel. " + tel + " adres: " + city;
+        int l = sentence.length();
+
+        for (int i=1; i<=sentence.length(); i++){
+frame += "*";
+        }
+            System.out.println(frame);
+            System.out.println("* " + name + " " + surname);
+            System.out.println(sentence);
+            System.out.println(frame);
+         
+    }   
 }
