610e9454183e67d6d86d4b19ab8851ad7f3017a5
olga-kacala <olga.kacala@gmail.com>
2023-11-17 17:31:49 +0100

    average exe done

--- Java/Java_Olga_Kacala/src/Average.java
+++ Java/Java_Olga_Kacala/src/Average.java
@@ -1,3 +1,37 @@
+import java.util.Scanner;
+
 public class Average {
+
+    public static double average(int[] marks) {
+        int sum = 0;
+
+        for (int i = 0; i < marks.length; i++) {
+            sum += marks[i];
+        }
+
+        return sum / (double) marks.length;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj ilość ocen ucznia:");
+        int marksAmount = scanner.nextInt();
+        scanner.nextLine();
+
+        int[] marksArray = new int[marksAmount];
+
+        System.out.println("Podaj oceny:");
+
+        for (int i = 0; i < marksAmount; i++) {
+            marksArray[i] = scanner.nextInt();
+            scanner.nextLine();
+        }
+
+        double result = average(marksArray);
+        System.out.println("Śrenia z ocen ucznia: " + result);
+        scanner.close();
+    }
 
 }
