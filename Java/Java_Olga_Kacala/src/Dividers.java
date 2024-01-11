f0e15803a9aaba1c7745c78cba077f4debbb2779
olga-kacala <olga.kacala@gmail.com>
2024-01-11 18:32:55 +0100

    dividers

--- Java/Java_Olga_Kacala/src/Dividers.java
+++ Java/Java_Olga_Kacala/src/Dividers.java
@@ -1,17 +1,44 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Dividers {
 
     public static void main(String[] args) {
         int number = readNumber();
-
+        int[] dividers = findDividers(number);
+        printDividers(dividers);
     }
 
     public static int readNumber() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Pls give number: ");
+        System.out.println("Enter natural number: ");
         return scanner.nextInt();
+    }
 
+    public static boolean checkDividers(int a, int b) {
+        return a % b == 0;
+    }
+
+    public static int[] findDividers(int x) {
+        int dividers[] = new int[x / 2 + 1];
+        int count = 0;
+
+        for (int i = 1; i <= x / 2; i++) {
+            if (checkDividers(x, i)) {
+                dividers[count] = i;
+                count++;
+            }
+        }
+        dividers[count] = x;
+        count++;
+        return Arrays.copyOf(dividers, count);
+    }
+
+    public static void printDividers(int[] dividers) {
+        System.out.println("Dividers: ");
+        for (int divider : dividers) {
+            System.out.print(divider + ", ");
+        }
     }
 
 }
